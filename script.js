3857d7731f036dc93d043e13b8bd5f31eb0b8eac
Prince <princeraj.chauhan@outlook.com>
2023-06-20 17:00:42 +0530

    Initial complete

---
+++ script.js
@@ -0,0 +1,73 @@
+
+
+
+// ************************** Question 1 ***************************
+
+let code1 = document.getElementById("Code1")
+let isCode1 = false
+function seeHide1(element) {
+    if (isCode1) {
+        element.innerText = "See Code"
+        code1.style.display = "none"
+        isCode1 = false
+    }
+    else{
+        element.innerText = "Hide Code"
+        code1.style.display = "block"
+        isCode1 = true
+    }
+}
+
+
+// ************************** Question 2 ***************************
+
+let code2 = document.getElementById("Code2")
+let isCode2 = false
+function seeHide2(element) {
+    if (isCode2) {
+        element.innerText = "See Code"
+        code2.style.display = "none"
+        isCode2 = false
+    }
+    else{
+        element.innerText = "Hide Code"
+        code2.style.display = "block"
+        isCode2 = true
+    }
+}
+
+
+// ************************** Question 3 ***************************
+
+let code3 = document.getElementById("Code3")
+let isCode3 = false
+function seeHide3(element) {
+    if (isCode3) {
+        element.innerText = "See Code"
+        code3.style.display = "none"
+        isCode3 = false
+    }
+    else{
+        element.innerText = "Hide Code"
+        code3.style.display = "block"
+        isCode3 = true
+    }
+}
+
+
+// ************************** Question 4 ***************************
+
+let code4 = document.getElementById("Code4")
+let isCode4 = false
+function seeHide4(element) {
+    if (isCode4) {
+        element.innerText = "See Code"
+        code4.style.display = "none"
+        isCode4 = false
+    }
+    else{
+        element.innerText = "Hide Code"
+        code4.style.display = "block"
+        isCode4 = true
+    }
+}
